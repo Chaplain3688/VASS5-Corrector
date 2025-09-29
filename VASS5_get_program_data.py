3a7b4296a640887c423eeafe990bd3d0b5db58ad
Chaplain3688 <inzunza.ernesto@gmail.com>
2025-09-29 16:16:07 -0600

    feat: Add Store program data function

---
+++ VASS5_get_program_data.py
@@ -0,0 +1,66 @@
+import os
+import pandas as pd
+import ast
+import VASS5_patterns as vassp
+import VASS5_main as vassm
+
+robotsdata_filename_path = os.path.join(vassm.input_path, "robots_data.xlsx")
+
+df_robots = pd.read_excel(robotsdata_filename_path)
+
+#Read the content of a file and divide it into lines
+def read_program_file(file_name):
+    with open(file_name, "r") as f:
+        content = f.read() 
+    lines = content.splitlines()
+    return lines
+
+def get_program_main_data(program_lines):
+    program_data = {
+        "Program Name": None,
+        "Comment": None,
+        "File Name": None
+    }
+
+    for line in program_lines:
+        prog_name_match = vassp.program_name_pattern.match(line)
+        if prog_name_match:
+            program_data["Program Name"] = prog_name_match.group(1).strip()
+            continue
+
+        comment_match = vassp.program_comment_pattern.match(line)
+        if comment_match:
+            program_data["Comment"] = comment_match.group(1).strip()
+            continue
+
+        file_name_match = vassp.program_filename_pattern.match(line)
+        if file_name_match:
+            program_data["File Name"] = file_name_match.group(1).strip()
+            continue
+
+    return program_data
+
+def safe_literal_eval(s):
+    try:
+        return ast.literal_eval(s)
+    except (ValueError, SyntaxError):
+        # Return an empty list if the string is malformed or not a list
+        return []
+
+def store_program_data(robots):
+
+    all_programs_data = []
+    for robot in robots:
+        robot_id = robot["robot_id"]
+        for program_name in robot["Programs"]:
+            row_data = {
+            "robot_id": robot_id,
+            "program_id": program_name,
+            "Program": program_name
+        }
+        
+            all_programs_data.append(row_data)
+
+    programs_df = pd.DataFrame(all_programs_data)
+    return programs_df
+
--- VASS5_main.py
+++ VASS5_main.py
@@ -3,12 +3,14 @@ import fnmatch
 import re
 import pandas as pd
 import VASS5_patterns as vassp
+import VASS5_get_program_data as vassg
 
 input_path = "C:\\Users\\inzun\\OneDrive\\Persona Fisica\\03 Proyectos\\Puebla VW\\VW371 Jetta\\05 Tasks\\2025 CW40 USTW5\\Correciones_USTW5"
 robotsdata_filename_path = os.path.join(input_path, "robots_data.xlsx")
-robots= []
+robotsdata_filename_path = os.path.join(input_path, "programs_list.xlsx")
 
-def store_robots(input_path, robots):
+def store_robots(input_path):
+    robots= []
     for root, ____, files in os.walk(input_path):
         for file in files:
             robot_full_name = os.path.basename(root)
@@ -19,10 +21,15 @@ def store_robots(input_path, robots):
                 continue
 
             match_robot = re.search(vassp.robot_name_pattern, robot_full_name)
+            if match_robot is None:
+                continue
+
             line = ('K'+ match_robot.group(1))
+            robot_id = (match_robot.group(1)[1:] + match_robot.group(2) + match_robot.group(4) + 'R' + match_robot.group(5))
             robot_name = (match_robot.group(4) + 'R' + match_robot.group(5))
             
             robot = {
+    "robot_id": robot_id,
     "Robot Full Name": robot_full_name,
     "Line": line,
     "ARG": match_robot.group(2),
@@ -34,17 +41,15 @@ def store_robots(input_path, robots):
 }
             
             robots.append(robot)
-
-#Read the content of a file and divide it into lines
-def read_program_file(file_name):
-    with open(file_name, "r") as f:
-        content = f.read() 
-    lines = content.splitlines()
-    return lines
+    return robots
 
 if __name__ == "__main__":
 
-    store_robots(input_path, robots)
+    robots = store_robots(input_path)
     robots_df = pd.DataFrame(robots)
     robots_df.to_excel(robotsdata_filename_path, index=False)
+
+    programs_list = vassg.store_program_data(robots)
+    programs_list_df = pd.DataFrame(programs_list)
+    programs_list_df.to_excel(robotsdata_filename_path, index=False)
 
