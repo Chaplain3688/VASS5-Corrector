6b1804691c8e2ee4776aec7499b1845f3957e296
Chaplain3688 <inzunza.ernesto@gmail.com>
2025-09-29 14:16:14 -0600

    refactor: Change name for main program, add a new module to get program data.

--- VASS5_main.py
+++ VASS5_main.py
@@ -6,7 +6,6 @@ import VASS5_patterns as vassp
 
 input_path = "C:\\Users\\inzun\\OneDrive\\Persona Fisica\\03 Proyectos\\Puebla VW\\VW371 Jetta\\05 Tasks\\2025 CW40 USTW5\\Correciones_USTW5"
 robotsdata_filename_path = os.path.join(input_path, "robots_data.xlsx")
-search_pattern = "folge05?.ls"
 robots= []
 
 def store_robots(input_path, robots):
@@ -36,49 +35,12 @@ def store_robots(input_path, robots):
             
             robots.append(robot)
 
-#Find all folges for Jetta in the specified directory and its subdirectories
-def search_for_jetta_folges(input_path, search_pattern):
-    folges_jetta = []
-
-    for root, dirs, files in os.walk(input_path):
-        for file in files:
-            filename = file.lower()
-            if fnmatch.fnmatch(filename, search_pattern):
-                full_path = os.path.join(root, filename)
-                folges_jetta.append(full_path)
-
-    return folges_jetta
-
 #Read the content of a file and divide it into lines
-def read_folge_file(file_name):
+def read_program_file(file_name):
     with open(file_name, "r") as f:
         content = f.read() 
     lines = content.splitlines()
     return lines
-
-def get_program_data(lines, robots):
-    program_data = {
-        "Program Name": None,
-        "Comment": None,
-        "File Name": None
-    }
-
-    for line in lines:
-        prog_name_match = vassp.program_name_pattern.match(line)
-        if prog_name_match:
-            program_data["Program Name"] = prog_name_match.group(1).strip()
-            continue
-
-        comment_match = vassp.program_comment_pattern.match(line)
-        if comment_match:
-            program_data["Comment"] = comment_match.group(1).strip()
-            continue
-
-        file_name_match = vassp.program_filename_pattern.match(line)
-        if file_name_match:
-            program_data["File Name"] = file_name_match.group(1).strip()
-            continue
-
 
 if __name__ == "__main__":
 
@@ -86,5 +48,3 @@ if __name__ == "__main__":
     robots_df = pd.DataFrame(robots)
     robots_df.to_excel(robotsdata_filename_path, index=False)
 
-
-    folges_jetta = search_for_jetta_folges(input_path, search_pattern)
