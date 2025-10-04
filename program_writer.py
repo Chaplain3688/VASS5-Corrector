b31473e3adb0c5c8253c6aabc86b906d42a7b951
Chaplain3688 <inzunza.ernesto@gmail.com>
2025-10-03 20:58:45 -0600

    feat: Add point with parameters to the write_main_section
    Now the complete point with its parameters its written by the function, aslo the attributes section function is with the right tabs and spaces

--- program_writer.py
+++ program_writer.py
@@ -37,26 +37,26 @@ def write_attributes_section(program):
     lines = []
     lines.append("/PROG  " + program["Program Name"])
     lines.append("/ATTR")
-    lines.append("OWNER      = " + program["OWNER"] + ";")
-    lines.append("COMMENT    = \"" + program["COMMENT"] + "\";")
-    lines.append("PROG_SIZE  = " + str(program["PROG_SIZE"]) + ";")
-    lines.append("CREATE     = DATE " + program["CREATE"][:9] + "  TIME " + program["CREATE"][9:] + ";")
-    lines.append("MODIFIED   = DATE " + program["MODIFIED"][:9] + "  TIME " + program["MODIFIED"][9:] + ";")
-    lines.append("FILE_NAME  = " + program["FILE_NAME"] + ";")
-    lines.append("VERSION    = " + str(program["VERSION"]) + ";")
-    lines.append("LINE_COUNT = " + str(program["LINE_COUNT"]) + ";")
-    lines.append("MEMORY_SIZE= " + str(program["MEMORY_SIZE"]) + ";")
-    lines.append("PROTECT    = " + program["PROTECT"] + ";")
+    lines.append("OWNER\t\t= " + program["OWNER"] + ";")
+    lines.append("COMMENT\t\t= \"" + program["COMMENT"] + "\";")
+    lines.append("PROG_SIZE\t= " + str(program["PROG_SIZE"]) + ";")
+    lines.append("CREATE\t\t= DATE " + program["CREATE"][:9] + " TIME " + program["CREATE"][9:] + ";")
+    lines.append("MODIFIED\t= DATE " + program["MODIFIED"][:9] + " TIME " + program["MODIFIED"][9:] + ";")
+    lines.append("FILE_NAME\t= " + program["FILE_NAME"] + ";")
+    lines.append("VERSION\t\t= " + str(program["VERSION"]) + ";")
+    lines.append("LINE_COUNT\t= " + str(program["LINE_COUNT"]) + ";")
+    lines.append("MEMORY_SIZE\t= " + str(program["MEMORY_SIZE"]) + ";")
+    lines.append("PROTECT\t\t= " + program["PROTECT"] + ";")
     if program["STORAGE"]:
-        lines.append("STORAGE    = " + program["STORAGE"] + ";")
-    lines.append("TCD:  STACK_SIZE = " + str(program["STACK_SIZE"]) + ",")
-    lines.append("      TASK_PRIORITY = " + str(program["TASK_PRIORITY"]) + ",")
-    lines.append("      TIME_SLICE = " + str(program["TIME_SLICE"]) + ",")
-    lines.append("      BUSY_LAMP_OFF = " + str(program["BUSY_LAMP_OFF"]) + ",")
-    lines.append("      ABORT_REQUEST = " + str(program["ABORT_REQUEST"]) + ",")
-    lines.append("      PAUSE_REQUEST = " + str(program["PAUSE_REQUEST"]) + ";")
-    lines.append("DEFAULT_GROUP = " + program["DEFAULT_GROUP"] + ";")
-    lines.append("CONTROL_CODE  = " + program["CONTROL_CODE"] + ";")
+        lines.append("STORAGE\t\t= " + program["STORAGE"] + ";")
+    lines.append("TCD:  STACK_SIZE\t= " + str(program["STACK_SIZE"]) + ",")
+    lines.append("      TASK_PRIORITY\t= " + str(program["TASK_PRIORITY"]) + ",")
+    lines.append("      TIME_SLICE\t= " + str(program["TIME_SLICE"]) + ",")
+    lines.append("      BUSY_LAMP_OFF\t= " + str(program["BUSY_LAMP_OFF"]) + ",")
+    lines.append("      ABORT_REQUEST\t= " + str(program["ABORT_REQUEST"]) + ",")
+    lines.append("      PAUSE_REQUEST\t= " + str(program["PAUSE_REQUEST"]) + ";")
+    lines.append("DEFAULT_GROUP\t= " + program["DEFAULT_GROUP"] + ";")
+    lines.append("CONTROL_CODE\t= " + program["CONTROL_CODE"] + ";")
     
     output = "\n".join(lines)
     return output
@@ -87,27 +87,42 @@ def write_main_section(program, points_parameters, points_logic):
                         match_point.update(point)
                         program_points.append(match_point)
 
-
     lines = []
     lines.append("/MN")
 
     for point in program_points:
-        #print('robot_id:', point["robot_id"])
-        #print('program_id:', point["program_id"])
-        #print('point_id:', point["point_id"])
-        #print('Comments:', point["Comments"])
-        #print('Logic:', point["Logic"])
-        #time.sleep(5)
-
         if point["Comments"]:
             for comment in point["Comments"]:
                 lines.append(comment)
                 #print("Adding comment:", comment)
 
+        if not program["program_id"].lower().startswith("makro"):
+
+            spaces_line_number = 4 - int(len(point["Line Number"]))
+
+            point_parameter = ((spaces_line_number * " ") + str(point["Line Number"]) + ":" + 
+                               point["Movement Type"] + " P[" + 
+                               str(point["point_id"]) + "] " + 
+                               str(point["Speed Value"]) + 
+                               point["Speed Type"] + " " + 
+                               point["Continuity Type"] + 
+                               str(point["Continuity Value"]))
+
+
+            if point["ACC Type"]:
+                point_parameter += " ACC" + str(point["ACC Value"])
+
+            point_parameter += " " + point["TB/DB Type"] + "   " + str(point["TB/DB Value"]) + point["TB/DB Unit"] + point["Additional Parameters"]
+
+            lines.append(point_parameter + ";")
+
         if point["Logic"]:
             for logic in point["Logic"]:
                 lines.append(logic)
                 #print("Adding logic:", logic)
+
+        if not program["program_id"].lower().startswith("makro"):
+            lines.append("       ------ ;")
 
     output = "\n".join(lines)
     return output
